edae6760e8fabb0b254adf71f3917e18a6b9e937
OfficialCRUGG <ich@crugg.de>
2023-07-18 04:06:24 +0200

    chore: remove animation from demo page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,6 @@
 </script>
 
 <h1>svelte-skinview3d</h1>
-<Skinview3d
-  width="300"
-  height="600"
-  {skinUrl}
-  options={{
-    animation: new WalkingAnimation()
-  }}
-/>
+<Skinview3d width="300" height="600" {skinUrl} />
 <button on:click={() => (uuid = "1279b4d9-cd87-487a-b812-dee3c7aae15b")}>CRUGG</button>
 <button on:click={() => (uuid = "f883d0cd-917c-4843-8078-fe176e3f9a9e")}>_Trasher</button>
